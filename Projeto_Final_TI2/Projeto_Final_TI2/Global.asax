49e67e63e15fe3f45b72b9070e2bf1edbf3f791f
Gonçalo <goncalorocha98@gmail.com>
2019-04-10 00:27:02 +0100

    Criação do projeto de TI2 sobre Hoteis.

---
+++ Projeto_Final_TI2/Projeto_Final_TI2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Projeto_Final_TI2.MvcApplication" Language="C#" %>
---
+++ Projeto_Final_TI2/Projeto_Final_TI2/Models/Hoteis.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Web;
+
+namespace Projeto_Final_TI2.Models
+{
+    public class Hoteis 
+    {
+        //id dos hoteis
+        public int Id { get; set; }
+    }
+}

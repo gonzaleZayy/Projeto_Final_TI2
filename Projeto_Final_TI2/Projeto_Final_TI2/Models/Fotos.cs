16d1d9282db12f87a9add6be9a25f88b8b3d84f8
Gonçalo <goncalorocha98@gmail.com>
2019-04-10 01:08:46 +0100

    Adição da tabela Fotos

---
+++ Projeto_Final_TI2/Projeto_Final_TI2/Models/Fotos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web;
+
+namespace Projeto_Final_TI2.Models
+{
+    public class Fotos
+    {
+        //string com nome do ficheiro
+        public string nomeFicheiro { get; set; }
+        //legenda das fotos
+        public string legenda { get; set; }
+
+    }
+}

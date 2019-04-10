a3fee587174c90553b87e8cef425ca8d135326cb
Gonçalo <goncalorocha98@gmail.com>
2019-04-10 23:12:16 +0100

    Todas as tabelas necessárias já colocadas.

---
+++ Projeto_Final_TI2/Projeto_Final_TI2/Models/Clientes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Web;
+
+namespace Projeto_Final_TI2.Models
+{
+    public class Clientes
+    {
+        //id do cliente
+        public int IdCliente { get; set; }
+        //localidade do cliente
+        public string Localidade { get; set; }
+        //data de nascimento do cliente
+        public DateTime DataNasc { get; set; }
+        //morada do cliente
+        public string Morada { get; set; }
+        //nome do cliente
+        public string NomeCliente { get; set; }
+    }
+}
--- Projeto_Final_TI2/Projeto_Final_TI2/Models/Fotos.cs
+++ Projeto_Final_TI2/Projeto_Final_TI2/Models/Fotos.cs
@@ -5,6 +5,8 @@ namespace Projeto_Final_TI2.Models
 {
     public class Fotos
     {
+        //id da foto
+        public int IdFoto { get; set; }
         //string com nome do ficheiro
         public string nomeFicheiro { get; set; }
         //legenda das fotos
